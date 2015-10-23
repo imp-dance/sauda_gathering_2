43e42acca27fcb5d56ac5114332f90e658fd1e23
Moortiii <Morten.hauge97@gmail.com>
2015-10-23 21:52:27 +0200

    Fixa litt i columns

--- index.php
+++ index.php
@@ -8,67 +8,50 @@ include("header.php"); ?>
         <?php
     } ?>
     <div class="super_container">
-    <div class="jumbotron">
+     <div class="jumbotron">
       <div class="container">
-<<<<<<< HEAD
-        <h1>Velkommen til <br> Sauda Gathering</h1>
-        <p><a class="btn btn-success btn-lg" href="register.php" role="button">Meld deg på her &raquo;</a></p>
-=======
-        <h1>Velkommen til <br> Sauda Gathering!!</h1>
+       <h1>Velkommen til <br> Sauda Gathering!!</h1>
         <p class="inlinecenter"><a class="btn btn-success btn-lg" href="register.php" role="button">Meld deg på her &raquo;</a></p>
->>>>>>> 6818c940e6c30d79f751049a964123ddd7307e57
       </div>
-    </div>
-    <div class="container">
-      <!-- Example row of columns -->
+     </div>
+     <!--Her begynne columns med nyhetssaker -->
+      <div class="container">
       <div class="row">
         <div class="col-md-4">
-		 <div class="panel panel-primary">
-			<div class="panel-heading">
-				<h3 class="panel-title">Sauda Gathering 2015</h3>
-			</div>
-		  <div class="panel-body">
-          <p class="panel-content">Sauda Gathering 2015 blir arrangert fra lørdag 3. til onsdag 7. oktober. Dette er ett rusfritt arrangement rettet mot ungdom fra ungdomsskulealder og oppover.
-<br><br>
-Sted: Saudahallen i Sauda, Rogaland.
-<br>
-Tid: 3. oktober klokka 12:00 til 7. oktober klokka 12.
-<br>
-Plasser: 150**
-<br><br>
-Vilkår for påmelding og reglene for arrangementet finner du her: <a href="#">Regler</a>
-<br><br>
-Priser finner du her: <a href="#">Prisliste</a>
-<br><br>
-**Vi har plass til 150 deltakere i tillegg til at hver deltaker kan dele plassen sin med én person.
-<br><br>
-Det vil i tillegg bli solgt dagsbilletter og "ukespass". Dette er kun for adgang til hallen i tidsrommet 08:00 til 23:00. <a href="#">Påmelding</a>
-		  </p>
-		  </div>
-        </div>
+    		 <div class="panel panel-primary">
+    			<div class="panel-heading">
+    				<h3 class="panel-title">Sauda Gathering 2015</h3>
+    			</div>
+    		 <div class="panel-body">
+         <div class="panel-content">
+            <p>Sauda Gathering 2015 blir arrangert fra lørdag 3. til onsdag 7. oktober. Dette er ett rusfritt arrangement rettet mot ungdom fra ungdomsskulealder og oppover.</p>
+            <p>Sted: Saudahallen i Sauda, Rogaland.</p>
+            <p>Tid: 3. oktober klokka 12:00 til 7. oktober klokka 12.</p>
+            <p>Plasser: 150**</p>
+            <p>Vilkår for påmelding og reglene for arrangementet finner du her: <a href="#">Regler</a></p>
+            <p>Priser finner du her: <a href="#">Prisliste</a></p>
+            <p>**Vi har plass til 150 deltakere i tillegg til at hver deltaker kan dele plassen sin med én person.</p>
+            <p>Det vil i tillegg bli solgt dagsbilletter og "ukespass". Dette er kun for adgang til hallen i tidsrommet 08:00 til 23:00. <a href="#">Påmelding</a></p>
+         </div>
+		    </div>
        </div>
-        <div class="col-md-4">
-		 <div class="panel panel-primary">
-			<div class="panel-heading">
-				<h3 class="panel-title">Planleggingen er i gang</h3>
-			</div>
-		  <div class="panel-body">
-          <p class="panel-content">Planlegging av årets treff er i gang for fullt. Datoen for treffet er satt 3. til 7. oktober, altså i høstferien. Planlagte compoer så langt er:
-          <br><br>
-          Counter Strike: Global Offensive
-          <br><br>
-          League of Legends
-          <br><br>
-          Hearthstone
-          <br><br>
-          Rocket League
-          <br><br>
-          FIFA
-          <br><br>
-          Trackmania
-          <br><br>
-          Savnet du noe på treffet i fjor eller har tips til oss i forbindelse med planleggingen, ta gjerne kontakt! Kontaktinformasjon finner du nederst på siden eller ved å trykke <a href="#">her</a>.
-          </p>
+      </div>
+       <div class="col-md-4">
+    		<div class="panel panel-primary">
+    		 <div class="panel-heading">
+    		  <h3 class="panel-title">Planleggingen er i gang</h3>
+    		 </div>
+    		  <div class="panel-body">
+          <div class="panel-content">
+          <p>Planlegging av årets treff er i gang for fullt. Datoen for treffet er satt 3. til 7. oktober, altså i høstferien. Planlagte compoer så langt er:</p>
+          <p>Counter Strike: Global Offensive</p>
+          <p>League of Legends</p>
+          <p>Hearthstone</p>
+          <p>Rocket League</p>
+          <p>FIFA</p>
+          <p>Trackmania</p>
+          <p>Savnet du noe på treffet i fjor eller har tips til oss i forbindelse med planleggingen, ta gjerne kontakt! Kontaktinformasjon finner du nederst på siden eller ved å trykke <a href="#">her</a>.</p>
+      </div>
 		  </div>
         </div>
        </div>	
@@ -78,8 +61,10 @@ Det vil i tillegg bli solgt dagsbilletter og "ukespass". Dette er kun for adgang
 				<h3 class="panel-title">Data dag og natt	</h3>
 			</div>
 		  <div class="panel-body">
-          <p class="panel-content">I dag fikk Sauda Gathering besøk av Ryfylke. De ønsket å ta noen bilder og skrev en sak i avisen, blant annet om planleggingen av treffet og om hvorfor vi velger å arrangere.<br><br>Saken innehold også et intervju med Johannes Frøystad og Glenn-Andre Risvold Kalvik. <br><br> Ønsker du å lese hele artikkelen finner du den i papirutgaven av ryfylke eller på ryfylkes nettsider <a href="#">her</a>.
-		  </p>
+          <div class="panel-content">
+            <p>I dag fikk Sauda Gathering besøk av Ryfylke. De ønsket å ta noen bilder og skrev en sak i avisen, blant annet om planleggingen av treffet og om hvorfor vi velger å arrangere.</p>
+            <p>Saken innehold også et intervju med Johannes Frøystad og Glenn-Andre Risvold Kalvik. <br><br> Ønsker du å lese hele artikkelen finner du den i papirutgaven av ryfylke eller på ryfylkes nettsider <a href="#">her</a>.</p>
+		  </div>
 		  </div>
         </div>
        </div>
