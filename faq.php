8aab0e5b695f4632be6c47bbce59c86f29c0e32b
Moortiii <Morten.hauge97@gmail.com>
2015-10-24 21:55:30 +0200

    FAQ kategoriar

--- faq.php
+++ faq.php
@@ -10,6 +10,9 @@
         <div class="container">
          	<div class="row">
          		<table class="faqt">
+                    <tr valign="top" class="faqheading">
+                        <td colspan="2" style="text-align: center;"><h2 style="font-size: 40px;">PÅMELDING</h2></td>
+                    </tr>
          			<tr valign="top">
          				<td><strong>Kan jeg bytte plass etter at jeg har betalt?</strong></td>
          				<td>Ja, betalingen og plassnummeret er uavhengige. Du kan bytte plass helt frem til rett før treffet starter.</td>
@@ -19,18 +22,29 @@
          				<td>Ja, det er mulig å betale med nettbank, men husk at medlemsnummer og navn må være med i betalingsinformasjon.
 Medlemsnummeret ditt finner du i "Endre profil" i "innstillinger" menyen.</td>
          			</tr>
-         			<tr valign="top">
-         				<td><strong>Hvordan bytter jeg nick?</strong></td>
-         				<td>For å bytte nick må du være pålogget.
-Trykk på "Endre profil" i innstillinger menyen og bytt nick helt nederst på siden.</td>
-         			</tr>
+                    <tr valign="top">
+                        <td><strong>Hvordan finner jeg medlemsnummeret mitt?</strong></td>
+                        <td>Det kan du finne ved å gå inn på linken "Endre profil" i menyen "innstillinger" til venstre. 
+Du kan også finner det under "Betalingsinfo" i samme meny.
+Pass på å få alt riktig skrevet!</td>
+                    </tr>
          			<tr valign="top">
          				<td><strong>Hvorfor får jeg ikke e-post?</strong></td>
          				<td>Noen kan oppleve problemer med at meldingene fra registreringsprosessen blir oppfattet som søppelpost/junkmail. Dette gjelder f.eks. de som bruker Hotmail. E-posten vil da havne i junkmail mappa.</td>
          			</tr>
+                    <tr valign="top" class="faqheading">
+                        <td colspan="2" style="text-align: center;"><h2 style="font-size: 40px;">MIN PROFIL</h2></td>
+                    </tr>
+                    <tr valign="top">
+                        <td><strong>Hvordan bytter jeg nick?</strong></td>
+                        <td>For å bytte nick må du være pålogget.
+Trykk på "Endre profil" i innstillinger menyen og bytt nick helt nederst på siden.</td>
+                    </tr>
          			<tr valign="top">
-         				<td><strong>Spørsmål?</strong></td>
-         				<td>En avatar er ikonet ditt som blir vist hver gang du skriver og sender en melding. Enten om det er på noens profilsiden til andre eller om det er på hovedsiden.<p>Grunnen til at man tar i bruk avatarer/ikoner er fordi det gjør det lettere å kjenne igjen folk, enn kun etter nicket.</p></td>
+         				<td><strong>Hvordan laster jeg opp et profilbilde?</strong></td>
+         				<td>For å laste opp et profilbilde går du inn på "Endre profil" og laster opp et bilde.
+
+NB! Bildet kan maks være på 100KB og MÅ være jpg. Anbefalt størrelse er 100x100 piksler. Bilder over denne størrelsen blir krympet.</p></td>
          			</tr>
          		</table>
          	</div>
