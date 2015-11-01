6a41b9c891ed13f12f7c531d5c0f52b51f437471
Håkon Underbakke <hakonunderbakke@Hakons-MacBook-Pro.local>
2015-11-01 15:55:04 +0100

    Create Tournament

---
+++ create_tournament.php
@@ -0,0 +1,22 @@
+<?php include("head.php");
+include("header.php");
+
+if (!empty($_POST)){
+  
+}
+
+?>
+<div class="super_container">
+  <div class="jumbotron">
+    <div class="container">
+      <h1>Lag Compo</h1>
+    </div>
+  </div>
+  <div class="container">
+    <form action="create_tournament.php" method="post">
+
+    </form>
+  </div> <!-- /container -->
+</div> <!-- /super_container -->
+<?php include("script.php");
+include("footer.php"); ?>
