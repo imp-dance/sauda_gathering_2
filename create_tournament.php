4b5791fe307a0fa29be18230b3c9f295c479de20
Håkon Underbakke <hakonunderbakke@Hakons-MacBook-Pro.local>
2015-11-01 16:16:35 +0100

    tiss

--- create_tournament.php
+++ create_tournament.php
@@ -2,7 +2,14 @@
 include("header.php");
 
 if (!empty($_POST)){
-  
+  $params = array(
+  "tournament[name]" => "My Tourney",
+  "tournament[tournament_type]" => "single elimination",
+  "tournament[url]" => "my_toruney",
+  "tournament[description]" => "Challonge is <strong>AWESOME</strong>"
+  );
+$tournament = $c->makeCall("tournaments", $params, "post");
+$tournament = $c->createTournament($params);
 }
 
 ?>
@@ -14,7 +21,23 @@ if (!empty($_POST)){
   </div>
   <div class="container">
     <form action="create_tournament.php" method="post">
-
+    	<table>
+    		<tr>
+    			<td>Namn:</td>
+    			<td><input type="text" name="name" /></td>
+    		</tr>
+    		<tr>
+    			<td>Type:</td>
+    			<td>
+    				<select>
+    					<option value="single elimination">Single Elimination</option>
+    				</select>
+    			</td>
+    		</tr>
+    		<tr>
+    			<td colspan="2"><input type="submit" value="Lag Turnament" /></td>
+    		</tr>
+    	</table>
     </form>
   </div> <!-- /container -->
 </div> <!-- /super_container -->
