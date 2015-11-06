62f3d1ec2bc8d60120b35bc329867fce3434e599
Moortiii <Morten.hauge97@gmail.com>
2015-11-06 18:20:54 +0100

    About.php HERE WE GO, full width column på bunn

--- about.php
+++ about.php
@@ -13,7 +13,7 @@ include("header.php"); ?>
    <div class="panel panel-primary">
     <div class="panel-heading">
     <!-- Tittel på column -->
-    <h3 class="panel-title uppercase">Hvem?</h3>
+    <h3 class="panel-title uppercase">Hvem er vi?</h3>
     </div>
 		<div class="panel-body">
      <p class="panel-content">Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
@@ -26,7 +26,7 @@ include("header.php"); ?>
    <div class="panel panel-primary">
     <div class="panel-heading">
     <!-- Tittel på column -->
-    <h3 class="panel-title uppercase">Hva?</h3>
+    <h3 class="panel-title uppercase">Hva gjør vi?</h3>
     </div>
     <div class="panel-body">
      <p class="panel-content">Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
@@ -39,46 +39,7 @@ include("header.php"); ?>
    <div class="panel panel-primary">
     <div class="panel-heading">
     <!-- Tittel på column -->
-    <h3 class="panel-title uppercase">Hvor?</h3>
-    </div>
-    <div class="panel-body">
-     <p class="panel-content">Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-     Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-     </div> <!-- Panel-body end -->
-    </div> <!-- Panel-primary end -->
-   </div> <!-- Col-md-4 end -->
-<!-- 4. Column -->
-  <div class="col-md-4">
-   <div class="panel panel-primary">
-    <div class="panel-heading">
-    <!-- Tittel på column -->
-    <h3 class="panel-title uppercase">Hvor?</h3>
-    </div>
-    <div class="panel-body">
-     <p class="panel-content">Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-     Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-     </div> <!-- Panel-body end -->
-    </div> <!-- Panel-primary end -->
-   </div> <!-- Col-md-4 end -->
-<!-- 5. Column -->
-  <div class="col-md-4">
-   <div class="panel panel-primary">
-    <div class="panel-heading">
-    <!-- Tittel på column -->
-    <h3 class="panel-title uppercase">Hvor?</h3>
-    </div>
-    <div class="panel-body">
-     <p class="panel-content">Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-     Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-     </div> <!-- Panel-body end -->
-    </div> <!-- Panel-primary end -->
-   </div> <!-- Col-md-4 end -->
-<!-- 6. Column -->
-  <div class="col-md-4">
-   <div class="panel panel-primary">
-    <div class="panel-heading">
-    <!-- Tittel på column -->
-    <h3 class="panel-title uppercase">Hvor?</h3>
+    <h3 class="panel-title uppercase">Hvor gjør vi dette?</h3>
     </div>
     <div class="panel-body">
      <p class="panel-content">Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
@@ -87,6 +48,18 @@ include("header.php"); ?>
     </div> <!-- Panel-primary end -->
    </div> <!-- Col-md-4 end -->
   </div> <!-- Row end -->
+  <div>
+   <div class="panel panel-primary">
+    <div class="panel-heading">
+    <!-- Tittel på column -->
+    <h3 class="panel-title uppercase">Hvor gjør vi dette?</h3>
+    </div>
+    <div class="panel-body">
+     <p class="panel-content">Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+     Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+     </div> <!-- Panel-body end -->
+    </div> <!-- Panel-primary end -->
+   </div> <!-- Col-md-4 end -->
  </div> <!-- Container end -->
 </div> <!-- Supercontainer end -->
 <?php include("script.php");
