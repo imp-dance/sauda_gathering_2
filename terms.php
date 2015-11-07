8c434a759cea2f8d4d34f8fcd12a935573fd280f
Moortiii <Morten.hauge97@gmail.com>
2015-11-07 12:58:17 +0100

    Større font-size på terms.php. Ekstremt med code-cleanup i about.php og terms.php

--- terms.php
+++ terms.php
@@ -14,7 +14,7 @@ include("header.php"); ?>
             </div>
             <div class="panel-body">
                  <p class="panel-content">
-                <ol>
+                <ol class="terms-content">
                 <li>Treffet blir arrangert fra lørdag 1. til onsdag 5. oktober 2016.</li>
                 <li>Påmeldingen er bindande.</li>
                 <li>Om du betaler, men ikke bruker plassen din har du ikke rett på å få beløpet tilbakebetalt.</li>
@@ -42,7 +42,7 @@ include("header.php"); ?>
             </div>
             <div class="panel-body">
                  <p class="panel-content">
-                <ol>
+                <ol class="terms-content">
                 <li>Bruk av private høytalere er forbudt.</li>
                 <li>Det er ikke lov å nyte alkohol eller andre rusmidler inne i hallen.</li>
                 <li>Rusede personer vil bli avvist/utvist fra hallen.</li>
@@ -77,7 +77,7 @@ include("header.php"); ?>
             </div>
             <div class="panel-body">
                  <p class="panel-content">
-                <ol>
+                <ol class="terms-content">
                     <li>
                     Dette gjelder http://www.saudanett.org og http://www2.saudanett.org.
                     Administrator(ene) på siden forsøker å fjerne eller redigere alle støtende innlegg så fort som mulig, men det er umulig å overvåke alle innlegg. Du må anerkjenne at alle innlegg representerer den enkelte brukers syn og holdninger, og vil ikke stille administrator(ene) til ansvar for innholdet i innleggene, med unntak av deres egne innlegg.
@@ -99,86 +99,3 @@ include("header.php"); ?>
 </div> <!-- Supercontainer end -->
 <?php include("footer.php");
 include("script.php"); ?>
-
-
-
-
-
-
-
-                <!--
-                <h1 style="margin-top: 120px; margin-bottom: 50px;"> Vilkår og regler </h1>
-            </div>
-        </div>
-            <div class="container">
-                <div class="terms-cont">
-                    <h3 style="font-size: 40px; margin-bottom: 20px;"> Vilkår </h3>
-                <ol>
-                <li>Treffet blir arrangert frå lørdag 1. til onsdag 5. oktober 2016.</li>
-                <li>Påmeldinga er bindande.</li>
-                <li>Om du betaler, men ikkje bruker plassen din har du ikkje rett til å få pengane att.</li>
-                <li>Du må minst ha starta på ungdomsskulen får å kunne delta.</li>
-                <li>Når du melder deg på må du bekrefta mobilnummeret ditt innan ein time. Dersom du ikkje bekreftar mobilnummeret innan fristen blir påmeldinga annullert og du må melda deg på på nytt.</li>
-                <li>Ved å melde deg på godtar du at Sauda Gathering kan sende deg informasjon på SMS. Det vil kun bli sendt SMS som er relevant for påmelding og deltakelse på arrangementet. Alle SMS du mottar er kostnadsfrie. Nummeret blir ikke videreformidlet til tredjepart.</li>
-                <li>Deltakeravgift ved registrering på nettet: 600kr.
-                Dette forutsetter betaling i bank. <b>Husk å ta med kvittering!</b>
-                Ønsker du å betale når du ankommer vil prisen bli 650,-</li>
-                <li><p>Som påmeldt vil din plass vera reservert deg, betalt eller ikkje betalt, inn til 12 timer frå arrangementets start.</p>
-                    <p>Plasser som ikke er betalt for, eller deltakeren ikke har møtt opp kan etter 12 timer bli solgt på nytt ved behov. <p><b>Ta kontakt hvis du ikke har mulighet til å møte første dagen.</b></p> </p>
-                    <p>kontaktinfo: melding til volzon her på siden eller sms: 99498847</p></li>
-                <li>Skal du dele datamaskin med en venn er det kun den som er ansvarlig for maskinen som trenger å melde seg på, den andre melder seg på i døra med tillatelse fra den som er ansvarlig for maskinen. Prisen for person nummer to er 350kr.</li>
-                <li>Det er mulig å melda seg på via Internett heilt fram til 8. oktober.</li>
-                </ol>
-            </div>
-        </div>
-        <div class="container">
-                <div class="terms-cont">
-                    <h3 style="font-size: 40px; margin-bottom: 20px;"> Regler for Sauda Gathering </h3>
-                <ol>
-                <li>Bruk av private høgtalarar er forbudt.</li>
-                <li>Det er ikkje lov å nyta alkohol eller andre rusmiddel inne i hallen.</li>
-                <li>Rusa personar vil bli avvist/utvist frå hallen.</li>
-                <li>Det er ikkje lov å plaga folk. Vis særleg omsyn til folk som søv.</li>
-                <li>Klatreveggen skal ikkje brukast.</li>
-                <li>Sauda Gathering tek ikkje ansvar for skade eller tjuveri av verdisaker og utstyr. Pass difor særleg godt på mobiltelefon, penger, memorystick osv.
-                Det er lurt å merka alt privat utstyr med navn, sovepose, stol, PC..  </li>
-                <li>Garderobene skal ikkje brukast, men det er høve til å bada i Saudahallen i dei offentlege badetidene, sjå oppslag ved resepsjonen nede.</li>
-                <li>Ved inn og utpassering skal deltakarane sei frå til vakta.</li>
-                <li>Alle deltakarar skal følgje instrukser frå vaktene.</li>
-                <li>Søppel skal leggjast i søppeldunkene som står utplassert i hallen.</li>
-                <li>Når datatreffet er slutt skal plassen vera like ryddig som når du kom!</li>
-                <li>Nødutgangane skal ikkje blokkerast.</li>
-                <li>Nødutgangane skal normalt vera stengt og kun nyttast ved eventuell nødsituasjon.</li>
-                <li>Den som skader eller øydelegg inventar / bygningsdeler i Saudahallen eller privat utstyr som tilhøyrer andre, er personlig ansvarlig for erstatning av skaden.</li>
-                <li>Bråk og vold medfører utkasting.</li>
-                <li>Røyking inne er ikkje tillate.</li>
-                <li>Etter kl 24:00 blir dørene stengt og berre opna for utpassering.
-                Dørene blir opna att kl 08.00 neste dag.</li>
-                <li>Utstyr som tilhøyrer eller er administrert av Sauda Gathering skal ikkje rørast av andre, enn medlemmer av Sauda Gathering Crew.</li>
-                <li>Ved eventuelle problem, teknisk eller praktisk bed vi den enkelte deltakar å ta dette opp omgåande med leiinga for Sauda Gathering.</li>
-                <li>Nedlasting og visning av pornografisk eller anna støytande innhald er ikkje tillat.</li>
-                <li>Brudd på overnevnte reglar fører til utkasting fra arrangementet.</li>
-                </ol>
-            </div>
-        </div>
-        <div class="container">
-            <div class="terms-cont">
-                <h3 style="font-size: 40px;">Reglar for bruk av nettstedet Sauda Gathering</h3>
-                <ol>
-                    <li>
-                    Gjelder http://www.saudanett.org og http://www2.saudanett.org 
-                    Administrator(ene) på siden forsøker å fjerne eller redigere alle støtende innlegg så fort som mulig, men det er umulig å overvåke alle innlegg. Du må anerkjenne at alle innlegg representerer den enkelte brukers syn og holdninger, og vil ikke stille administrator(ene) til ansvar for innholdet i innleggene, med unntak av deres egne innlegg.
-                </li>
-                <li>
-                    Du anerkjenner at du ikke har anledning til å skrive støtende, uanstendig, vulgært, injurierende, hatsk, truende, pornografiske eller andre typer innlegg som kan være eller er i strid med gjeldende lovverk. Om du skriver denne type innlegg vil du bli øyeblikkelig og permanent utestengt fra nettstedet.
-                </li>
-                <li>
-                    Du godkjenner at administrator(ene) på nettstedet har rett til å fjerne, redigere, flytte eller stenge et hvert tema når de anser det nødvendig. Som bruker godtar du at all informasjon du oppgir blir lagret i en database. Denne informasjonen vil ikke bli utlevert til tredjepart utenfor nettstedet, men webmaster(ene) og administrator(ene) kan ikke stilles ansvarlig for hacking ol. som kan medføre tap av eller innsyn i databasen.
-                </li>
-                <li>
-                    Din e-postadresse brukes bare i forbindelse med registrerings-prosessen , og for å sende nytt passord dersom du ønsker/ber om det. Husk å oppgi korrekt e-postadresse, ellers vil ikke registreringen bli gjennomført.
-                </li>
-            </ol>
-            </div>
-        </div>
-    </div> -->
