b5558149a435eb68f8c47ce8c43d544aaa72e186
Moortiii <Morten.hauge97@gmail.com>
2015-11-19 17:23:41 +0100

    Imageborder 'om oss' + margin

--- footer.php
+++ footer.php
@@ -1,19 +1,5 @@
 <?php
-$melding = $_POST['melding'];
-$melding = strip_tags($melding);
-$emne = "SG-Kontakt".$_POST['emne'];
-$emne = strip_tags($emne);
-$fra = $_POST['email'];
-$til = 'sgnettcrew@gmail.com';
-
-// In case any of our lines are larger than 70 characters, we should use wordwrap()
-$melding = wordwrap($message, 70, "\r\n");
-
-$headers = 'From: $fra' . "\r\n" .
-    'Reply-To: $fra' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-mail($til, $emne, $melding, $headers);
+include("mailto.php");
 ?>
 <div id="footer">
  <div class="container">
---
+++ mailto.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($melding)){}
+$melding = $_POST['melding'];
+$melding = strip_tags($melding);
+$emne = "SG-Kontakt".$_POST['emne'];
+$emne = strip_tags($emne);
+$fra = $_POST['email'];
+$til = 'sgnettcrew@gmail.com';
+
+// In case any of our lines are larger than 70 characters, we should use wordwrap()
+$melding = wordwrap($melding, 70, "\r\n");
+
+$headers = 'From: $fra' . "\r\n" .
+    'Reply-To: $fra' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($til, $emne, $melding, $headers);
+?>
